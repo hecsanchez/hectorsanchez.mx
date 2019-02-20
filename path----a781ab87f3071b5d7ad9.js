eae7400c1101c73eca258e28fc2f2e7b74806fa5
Hector Sanchez <hola@hectorsanchez.mx>
2019-02-19 20:38:20 -0600

    updated home page

---
+++ path----a781ab87f3071b5d7ad9.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{173:function(e,t){e.exports={data:{allContentfulAsset:{edges:[{node:{id:"c1RIeCiMW8UuM0cQwmMmKwM",title:"logo",file:{url:"//images.ctfassets.net/efy8gj3lo7l2/1RIeCiMW8UuM0cQwmMmKwM/790877864e197da8ddf0e27c0da1ee82/logo.png"}}}]}},layoutContext:{}}}});
+//# sourceMappingURL=path----a781ab87f3071b5d7ad9.js.map
